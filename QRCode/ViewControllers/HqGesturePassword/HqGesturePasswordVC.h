bf8280af83ba3a17f02a6d639600908aed3bbecf
macpro <>
2018-01-09 18:21:47 +0800

    Gesture

---
+++ QRCode/ViewControllers/HqGesturePassword/HqGesturePasswordVC.h
@@ -0,0 +1,13 @@
+//
+//  HqGesturePasswordVC.h
+//  QRCode
+//
+//  Created by macpro on 2018/1/9.
+//  Copyright © 2018年 macpro. All rights reserved.
+//
+
+#import "SuperVC.h"
+
+@interface HqGesturePasswordVC : SuperVC
+
+@end
